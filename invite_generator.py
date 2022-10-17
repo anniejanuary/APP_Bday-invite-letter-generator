e9d794471e1f611aca90dbaf4c6ab5011cfd182f
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-10-17 18:24:36 +0200

    Update invite_generator.py

--- invite_generator.py
+++ invite_generator.py
@@ -1,11 +1,11 @@
 class InviteGenerator:
     def __init__(self):
-        self.names_to_add = []
+        self.invitee_names = []
 
     def accessing_names(self):
         names = open("invited_names.txt").readlines()  # accessing all lines from the .txt file
         for _ in range(len(names)):
-            self.names_to_add.append(names[_].strip()) # strip nextline "\n"
+            self.invitee_names.append(names[_].strip()) # strip nextline "\n"
 
     def adding_names_to_letter(self):
         text_to_replace = "[name]"
@@ -14,5 +14,5 @@ class InviteGenerator:
         template = open("starting_letter.txt").read()
 
         # Creating new letters with names from names_to_add:
-        for _ in range(len(self.names_to_add)):
-            open(f"./ReadyToSend/Letter_{self.names_to_add[_]}.txt", "w").write(template.replace(text_to_replace, self.names_to_add[_]))
+        for _ in range(len(self.invitee_names)):
+            open(f"./ReadyToSend/Invite_for_{self.invitee_names[_]}.txt", "w").write(template.replace(text_to_replace, self.invitee_names[_]))
